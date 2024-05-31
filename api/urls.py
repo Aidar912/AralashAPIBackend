ae77bdd55d6a9e79c0f04b8ced211aa86193ff71
Aidar <97720321+Aidar912@users.noreply.github.com>
2024-06-01 02:58:42 +0600

    Subscription API upd

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('subscriptions/<int:pk>/', SubscriptionDetailView.as_view(), name='subscription-detail'),
     path('subscriptions/<int:pk>/update/', SubscriptionUpdateView.as_view(), name='subscription-update'),
     path('subscriptions/<int:pk>/delete/', SubscriptionDeleteView.as_view(), name='subscription-delete'),
+    path('change-subscription/<int:pk>/', ChangeSubscriptionView.as_view(), name='change-subscription'),
+    path('subscription-history/', SubscriptionHistoryListView.as_view(), name='subscription-history-list'),
 ]
